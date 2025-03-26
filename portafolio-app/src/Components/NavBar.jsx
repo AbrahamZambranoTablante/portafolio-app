927c703784eb83c8c6321281cc5e5daf18d02f87
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2025-03-26 18:32:09 -0400

    adjusting darkmode lightmode

--- portafolio-app/src/Components/NavBar.jsx
+++ portafolio-app/src/Components/NavBar.jsx
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom"
-import '../Pages/CSS/NavBar.css'
-import React from "react"
+import { Link } from "react-router-dom";
+import "../Pages/CSS/NavBar.css";
+import React, { useEffect } from "react";
 
-export default function NavBar ({scrollToProjects, scrollToContact, scrollToBlog, darkMode, setDarkMode}) {
-
+export default function NavBar({ scrollToProjects, scrollToContact, scrollToBlog, darkMode, setDarkMode }) {
+    
     const scrollToTop = () => {
         window.scrollTo({ top: 0, behavior: "smooth" });
     };
@@ -11,10 +11,19 @@ export default function NavBar ({scrollToProjects, scrollToContact, scrollToBlog
     const toggleTheme = () => {
         setDarkMode(!darkMode);
     };
-    
+
+    // Apply dark mode class to body
+    useEffect(() => {
+        if (darkMode) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.remove("dark-mode");
+        }
+    }, [darkMode]);
+
     return (
         <>
-            <div className="navbar-container">
+            <div className={`navbar-container ${darkMode ? "dark-mode" : ""}`}>
                 <div className="navbar-sections">
                     <div className="logo">
                         <Link to={"/"}><img onClick={scrollToTop} style={{ cursor: "pointer" }} src="/azlogo.jpg" alt="Logo" /> </Link>
@@ -25,18 +34,16 @@ export default function NavBar ({scrollToProjects, scrollToContact, scrollToBlog
                         <button className="nav-link" onClick={scrollToProjects}>PROJECTS</button> 
                         <Link to={"/virtualagent"}><button className="nav-link">VIRTUAL AGENT</button> </Link>
                         <a href="/Resume_Abraham Zambrano Tablante.pdf" download="Abraham_Resume.pdf" className="nav-link">
-                            DONWLOAD MY RESUME
+                            DOWNLOAD MY RESUME
                         </a>
-                            {/* Toggle Switch */}
+                        {/* Toggle Switch */}
                         <label className="switch">
                             <input type="checkbox" checked={darkMode} onChange={toggleTheme} />
                             <span className="slider round"></span>
                         </label>   
                     </nav>
                 </div>
-
             </div>
-            
         </>
-    )
+    );
 }
